54b4c41fd542466e84d3a8f8d51a4b37458ec3ab
im6chen <im6chen@qq.com>
2022-04-12 17:51:28 +0800

    微调

--- main.go
+++ main.go
@@ -98,7 +98,7 @@ func main() {
 	count := 0
 	fmt.Println(count)
 	for _, row := range rows[1:] {
-		res, err := sql.Exec(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13]) //string格式
+		res, err := sql.Exec(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21], row[22], row[23], row[24]) //string格式
 		if err != nil {
 			log.Println(res, err)
 		} else {
